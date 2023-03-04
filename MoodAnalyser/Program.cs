af16f6e9333ec986abfa6effe803d72b4fa17145
Rajat <rajatprajapati09@gmail.com>
2023-03-04 19:33:01 +0530

    Mood Analyser Program File

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MoodAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome in Mood Analyser program");
+        
+        }
+
+    }
+}
